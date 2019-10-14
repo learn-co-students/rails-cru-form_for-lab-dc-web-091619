2adb8e26a5d32853e835434d5fbc77618b74fe74
nihaprezz <sergio13prez@gmail.com>
2019-10-14 12:04:30 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Genre.destroy_all
+
+
+#Artist
+drake = Artist.find_or_create_by(name: "Drake", bio: "Used to be a rapper")
+kendrick = Artist.find_or_create_by(name: "Kendrick", bio: "Real Rapper")
+bas = Artist.find_or_create_by(name: "Bas", bio: "Dreamville artist from London")
+
+#Genres 
+pop = Genre.find_or_create_by(name: "Pop")
+hiphop = Genre.find_or_create_by(name: "Hip Hop")
+alternative = Genre.find_or_create_by(name: "Alternative")
+
+#Songs 
+forever = Song.find_or_create_by(name: "Forever", artist_id: drake.id, genre_id: pop.id)
+damn = Song.find_or_create_by(name: "Damn", artist_id: kendrick.id, genre_id: alternative.id)
+tribe = Song.find_or_create_by(name: "Tribe", artist_id: bas.id, genre_id: hiphop.id)
+
+
+
